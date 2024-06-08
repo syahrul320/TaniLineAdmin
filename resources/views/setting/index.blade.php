fce043df1078a24289c3b8bb596592e03683298f
DEVELOPERTY <it.thohiryasin@gmail.com>
2024-06-08 21:11:21 +0800

    fix helpdesk

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -73,7 +73,7 @@
                         <div class="form-group has-feedback">
                             <b><label>Helpdesk</label></b>
                             <input type="text" class="form-control" value="{{ $setting->helpdesk }}"
-                                name="helpdesk }}">
+                                name="helpdesk">
                         </div>
                     </div>
                     <div class="card-footer text-center col-sm-12">
