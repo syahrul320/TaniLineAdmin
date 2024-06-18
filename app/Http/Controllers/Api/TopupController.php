d68315cb71c7fbd0ffadf22adf3d87682bc239bc
DEVELOPERTY <it.thohiryasin@gmail.com>
2024-06-18 14:44:08 +0800

    tes flip production

--- app/Http/Controllers/Api/TopupController.php
+++ app/Http/Controllers/Api/TopupController.php
@@ -17,7 +17,7 @@ class TopupController extends Controller
             $ch = curl_init();
             $secret_key = 'Basic ' . base64_encode('JDJ5JDEzJE1RNkdlS25vME9ZcFQ5Y3VHZS5HbU80RjdmSXpZNi5JV3c1ZjRYS1RVR3JWb0pnVUV1WHVp:'); // replace 'your_secret_key' with your actual secret key
 
-            curl_setopt($ch, CURLOPT_URL, "https://bigflip.id/big_sandbox_api/v2/pwf/bill");
+            curl_setopt($ch, CURLOPT_URL, "https://bigflip.id/api/v2/pwf/bill");
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
             curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
