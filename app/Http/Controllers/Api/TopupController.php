3caccd1e93337714cbab4d3e242b8b37c33ead7d
DEVELOPERTY <it.thohiryasin@gmail.com>
2024-06-18 14:52:09 +0800

    cek lagi lagi

--- app/Http/Controllers/Api/TopupController.php
+++ app/Http/Controllers/Api/TopupController.php
@@ -44,7 +44,7 @@ class TopupController extends Controller
             $response = curl_exec($ch);
             curl_close($ch);
 
-            // $dataResponse = json_decode($response);
+            echo json_decode($response);
 
             // $topup = new Topup();
             // $topup->id_user_merchant = $request->id_user_merchant;
