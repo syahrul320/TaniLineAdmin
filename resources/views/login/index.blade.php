fa4eb3e7e4df9213d80dcf3721a464823064139f
Syahrul Hidayat <syahrulhidayat320@gmail.com>
2024-06-20 16:09:32 +0800

    icon login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -18,6 +18,7 @@
     <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/logo-icon-2.png') }}">
     <style>
         body {
             width: 100%
