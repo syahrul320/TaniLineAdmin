8e4f662f5f000f5140a7f2051b935f61f827bc04
Syahrul Hidayat <syahrulhidayat320@gmail.com>
2024-07-05 13:02:45 +0800

    fix bugs

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -397,23 +397,15 @@ class TransaksiController extends Controller
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
-        try {
-            $transaksi = Transaksi::where('id', $id)->first();
-            $transaksi->status_transaksi = $request->status_transaksi;
-            $transaksi->save();
-            if($request->status_transaksi == 'batal'){
-                $user = User::where('id', $transaksi->id_user_pembeli)->first();
-                $user->saldo += $transaksi->biaya_admin;
-                $user->save();
-            } 
-    
-            return response()->json([
-                'message' => 'Status transaksi berhasil diubah',
-                'data' => $transaksi
-            ]);
-        } catch (\Throwable $th) {
-            return response()->json(['message' => $th->getMessage()]);
-        }
+
+        $transaksi = Transaksi::where('id', $id)->first();
+        $transaksi->status_transaksi = $request->status_transaksi;
+        $transaksi->save();
+
+        return response()->json([
+            'message' => 'Status transaksi berhasil diubah',
+            'data' => $transaksi
+        ]);
     }
 
     public function notifPesananDiterima($id)
